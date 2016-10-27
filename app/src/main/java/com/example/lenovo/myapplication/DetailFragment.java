02124a81f42e12273ce94cff970f47920062c84b
mohamed6996 <mohamed.ali6996@hotmail.com>
2016-10-28 00:16:22 +0200

    films reviews

--- app/src/main/java/com/example/lenovo/myapplication/DetailFragment.java
+++ app/src/main/java/com/example/lenovo/myapplication/DetailFragment.java
@@ -15,11 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
@@ -31,11 +29,11 @@ import org.json.JSONObject;
  * A simple {@link Fragment} subclass.
  */
 public class DetailFragment extends Fragment {
-    TextView overView, releaseDate, voteAvg;
+    TextView overView, releaseDate, voteAvg , review_field ;
     ImageView image_path;
-    Button trailer;
-    String trailer_id, key ;
-
+    Button trailer, review;
+    String trailer_id, key;
+    String review_content ;
 
 
     public DetailFragment() {
@@ -61,6 +59,7 @@ public class DetailFragment extends Fragment {
         voteAvg = (TextView) view.findViewById(R.id.vote_average);
         image_path = (ImageView) view.findViewById(R.id.img);
         trailer = (Button) view.findViewById(R.id.trailer);
+        review_field = (TextView) view.findViewById(R.id.review_field);
 
         trailer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,17 +71,35 @@ public class DetailFragment extends Fragment {
                 builder.append("/videos?api_key=1a618051961d7a730414257885f0d9d3&language=en-US");
                 String Full_id = builder.toString();
 
-                fetch_key(Full_id);
+                fetch_youtube_key(Full_id);
 
 
                 try {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + key ));
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + key));
                     startActivity(intent);
                 } catch (ActivityNotFoundException ex) {
                     Intent intent = new Intent(Intent.ACTION_VIEW,
                             Uri.parse("http://www.youtube.com/watch?v=" + key));
                     startActivity(intent);
                 }
+
+            }
+        });
+
+
+        review = (Button) view.findViewById(R.id.review);
+        review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                StringBuilder builder = new StringBuilder();
+                builder.append(" https://api.themoviedb.org/3/movie/");
+                builder.append(trailer_id);
+                builder.append("/reviews?api_key=1a618051961d7a730414257885f0d9d3&language=en-US");
+                String Full_id = builder.toString();
+
+                fetch_review(Full_id);
+                review_field.setText(review_content);
 
             }
         });
@@ -115,7 +132,7 @@ public class DetailFragment extends Fragment {
         return view;
     }
 
-    public void fetch_key(String id) {
+    public void fetch_youtube_key(String id) {
 
 
         StringRequest string = new StringRequest(Request.Method.GET, id, new Response.Listener<String>() {
@@ -139,6 +156,32 @@ public class DetailFragment extends Fragment {
         });
 
         MySingleton.getInstance(getActivity()).addToRequestQueue(string);
+
+    }
+
+    public void fetch_review(String id) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray array = jsonObject.getJSONArray("results");
+                    JSONObject first = array.getJSONObject(0);
+                    review_content = first.getString("content");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        MySingleton.getInstance(getActivity()).addToRequestQueue(stringRequest);
 
     }
 
